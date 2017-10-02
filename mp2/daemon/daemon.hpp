15bb2036d47cd2ba3875f3804a919e03d8842785
sixpluszero <russellspurs@gmail.com>
2017-10-02 15:21:32 -0500

    update structure

--- mp2/daemon/daemon.hpp
+++ mp2/daemon/daemon.hpp
@@ -9,19 +9,20 @@
 class Daemon(){
 private:
 	UDPSocket sock_service;
-	vector<Node> member_list; // No. = index in the vector
-	map<string, long long> contact_list; // <IP, timestamp>
+	map<int, Node> member_list; // No. = index in the vector
+	vector<Node> contact_list; // <IP, timestamp>
 	int self_index;
 	long long local_timestamp;
 	mutex lock;
 public:
 	Daemon();
-	updateContact(); 
+	updateContact();
+ 	timeout(); // check whether its neighbors are down	
 	heartbeat(); // send hb to contacts
 	join(); // send join request to introducer, receive membership list, initial contact list
 	leave(); // send leave message to contacts
 	receive(); // endless listen to all messages
 	updateMember();
 	start(); // initial work. Join & start receive
-	resolve(); // Only for introducer. Introduce a new member
+	resolve(); // only for introducer. introduce a new member
 }
--- mp2/node/node.hpp
+++ mp2/node/node.hpp
@@ -5,6 +5,6 @@ public:
 	string ip; //IP Address
 	long long join_timestamp; // Timestamp when joining.
     long long timestamp; // Last HB TimeStamp;
-
+		int id; // Position on virtual ring;
     string toString();
 }
