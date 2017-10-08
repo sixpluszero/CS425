d808f68216b645814080142f5379f61d2d8354c2
sixpluszero <russellspurs@gmail.com>
2017-10-08 16:28:39 -0500

    add command line tools

--- mp2/start.py
+++ mp2/start.py
@@ -5,5 +5,9 @@ def start(id):
     server = "jialin2@fa17-cs425-g59-%02d.cs.illinois.edu" % (id)
     os.system("ssh %s \'pkill server; rm -f server*.log; nohup ./mp2/server start > /dev/null 2>&1 & \' " % (server))
 
-for vid in range(1, 4 + 1):
-    start(vid)
+if (len(sys.argv) == 3):
+    for vid in range(int(sys.argv[1]), int(sys.argv[2])+1):
+        start(vid)
+else:
+    for vid in range(1, 10+1):
+        start(vid)
