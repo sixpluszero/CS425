73ea188c97fcb4a6aae3a7e97a831e02287238d1
sixpluszero <russellspurs@gmail.com>
2017-11-05 01:12:08 -0500

    add random

--- mp3/src/daemon_master.cpp
+++ mp3/src/daemon_master.cpp
@@ -148,32 +148,48 @@ void Daemon::fixReplication(){
     plog("start fixing replication");
     for (auto it = file_location.begin(); it != file_location.end(); it++) {
         if (it->second.size() == 4) continue;
-        int src_node = it->second.begin()->first;
-        string fname = it->first;
-        plog("fixing %s using replica at %d", fname.c_str(), src_node);
-        while (it->second.size() < 4 && member_list.size() >= 4) {
-            for (auto it_0 = member_list.begin(); it_0 != member_list.end(); it_0++) {
-                if (it->second.find(it_0->first) == it->second.end()) {
-                    // This is a data slot to fit in
-                    int dst_node = it_0->first;
-                    plog("fixing %s picking %d", fname.c_str(), dst_node);
-                    TCPSocket sock_(member_list[src_node].ip, BASEPORT+3);
-                    tcpSendString(&sock_, "copy;"+std::to_string(dst_node)+"/"+fname);
-                    string ack = tcpRecvString(&sock_);
-                    plog("node %d reply with %s", src_node, ack.c_str());
-                    if (ack == "success"){
-                        plog("add replication of %s from %d to %d", fname.c_str(), src_node, dst_node);
-                        string update = fname + "/" + std::to_string(dst_node) + "/" + std::to_string(unixTimestamp());
-                        newFileMappingLocation(update);
-                        for (auto it2 = master_list.begin(); it2 != master_list.end(); it2++) {
-                            if (it2->second == "Primary") continue;
-                            TCPSocket sock_m(member_list[it2->first].ip, BASEPORT+3);
-                            tcpSendString(&sock_m, "newfloc;"+update);
+        while (it->second.size() != 4) {
+            /* Randomly pick one node with data replica */
+            vector< int > cand;
+            for (auto it_0 = it->second.begin(); it_0 != it->second.end(); it_0++) {
+                cand.push_back(it_0->first);
+            }
+            std::random_shuffle(cand.begin(), cand.end());
+            int src_node = cand[0];
+            plog("replica src node is %d", src_node);
+            string fname = it->first;
+            plog("fixing %s using replica at %d", fname.c_str(), src_node);
+            while (it->second.size() < 4 && member_list.size() >= 4) {
+                /* Randomly pick a new data node */
+                cand.clear();
+                for (auto it_0 = member_list.begin(); it_0 != member_list.end(); it_0++) {
+                    cand.push_back(it_0->first);
+                }
+                std::random_shuffle(cand.begin(), cand.end());
+                for (auto it_0 = cand.begin(); it_0 != cand.end(); it_0++) {
+                    if (it->second.find(*it_0) == it->second.end()) {
+                        // This is a data slot to fit in
+                        int dst_node = *it_0;
+                        plog("replica dst node is %d", dst_node);
+                        plog("fixing %s picking %d", fname.c_str(), dst_node);
+                        TCPSocket sock_(member_list[src_node].ip, BASEPORT+3);
+                        tcpSendString(&sock_, "copy;"+std::to_string(dst_node)+"/"+fname);
+                        string ack = tcpRecvString(&sock_);
+                        plog("node %d reply with %s", src_node, ack.c_str());
+                        if (ack == "success"){
+                            plog("add replication of %s from %d to %d", fname.c_str(), src_node, dst_node);
+                            string update = fname + "/" + std::to_string(dst_node) + "/" + std::to_string(unixTimestamp());
+                            newFileMappingLocation(update);
+                            for (auto it2 = master_list.begin(); it2 != master_list.end(); it2++) {
+                                if (it2->second == "Primary") continue;
+                                TCPSocket sock_m(member_list[it2->first].ip, BASEPORT+3);
+                                tcpSendString(&sock_m, "newfloc;"+update);
+                            }
+                            break;
                         }
-                        break;
                     }
                 }
-            }
+            }    
         }
     }   
     plog(fileMappingToString().c_str());
